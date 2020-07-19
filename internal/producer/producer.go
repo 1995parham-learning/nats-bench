0e5fe5cf07802775312460207bc5ad72ed09b024
Parham Alvani <parham.alvani@gmail.com>
2020-07-20 02:23:08 +0430

    feat: Add Internal Package

---
+++ internal/producer/producer.go
@@ -0,0 +1,55 @@
+package producer
+
+import (
+	"time"
+
+	"github.com/1995parham/nats-bench/internal/common"
+	"github.com/1995parham/nats-bench/message"
+	"github.com/sirupsen/logrus"
+)
+
+// MsgsPerClient divides the number of messages by
+// the number of clients and tries to distribute them as evenly as possible.
+func MsgsPerClient(nMsgs, nClients int) []int {
+	var counts []int
+
+	if nClients == 0 || nMsgs == 0 {
+		return counts
+	}
+
+	counts = make([]int, nClients)
+
+	mc := nMsgs / nClients
+
+	for i := 0; i < nClients; i++ {
+		counts[i] = mc
+	}
+
+	extra := nMsgs % nClients
+	for i := 0; i < extra; i++ {
+		counts[i]++
+	}
+
+	return counts
+}
+
+func Run(url, subject string, msgCount, msgSize int) {
+	c := common.Connect(url)
+	defer c.Close()
+
+	var msg message.Message
+
+	if msgSize > 0 {
+		msg.Payload = make([]byte, msgSize)
+	}
+
+	msg.Time = time.Now()
+
+	for i := 0; i < msgCount; i++ {
+		if err := c.Publish(subject, msg); err != nil {
+			logrus.Error(err)
+		}
+	}
+
+	c.Flush()
+}
---
+++ internal/subscriber/subscriber.go
@@ -0,0 +1,74 @@
+package subscriber
+
+import (
+	"log"
+	"time"
+
+	"github.com/1995parham/nats-bench/internal/common"
+	"github.com/1995parham/nats-bench/message"
+	"github.com/1995parham/nats-bench/sample"
+)
+
+type metrics struct {
+	tripTime time.Duration
+	size     int
+}
+
+func Run(url, subject string, msgCount int, timeout time.Duration) sample.Sample {
+	c := common.Connect(url)
+	defer c.Close()
+
+	ch := make(chan metrics)
+
+	if _, err := c.Subscribe(subject, func(m *message.Message) {
+		go func() {
+			ch <- metrics{
+				tripTime: time.Since(m.Time),
+				size:     len(m.Payload),
+			}
+		}()
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	var att time.Duration
+
+	var start time.Time
+
+	counter := 0
+	over := false
+	asize := 0
+
+	for {
+		ticker := time.NewTicker(timeout)
+		select {
+		case m := <-ch:
+			att += m.tripTime
+			asize += m.size
+			counter++
+
+			if counter == 1 {
+				start = time.Now()
+			}
+		case <-ticker.C:
+			over = true
+		}
+		ticker.Stop()
+
+		if counter >= msgCount {
+			counter = msgCount
+			break
+		}
+
+		if over {
+			break
+		}
+	}
+
+	return sample.Sample{
+		TripTime:         att / time.Duration(counter),
+		MessagePerSecond: float64(counter) / time.Since(start).Seconds(),
+		BytesPerSecond:   float64(asize) / time.Since(start).Seconds(),
+		Duration:         time.Since(start),
+	}
+}
